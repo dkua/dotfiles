b58e944aacf82e515c0191a360ec6ac34900981e
David Kua <david@vmfarms.com>
2013-06-05 12:32:00 -0400

    Added dotfiles.sh

---
+++ dotfiles.sh
@@ -0,0 +1,11 @@
+alias "tmux.new"='tmux new-session -s ${1}'
+alias "tmux.attach"='tmux attach-session -t ${1}'
+alias "tmux.list"='tmux list-sessions'
+alias "tmux.nuke"='tmux kill-server'
+
+git submodule init
+git submodule update
+
+ln -s tmux.conf ~/.tmux.conf
+ln -s vimrc ~/.vimrc
+ln -s vim ~/.vim
